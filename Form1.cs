37bfae82c2d3c56551cfd202f8b888365cd5d0c9
Asheryyy <24521557@gm.uit.edu,vn>
2025-09-28 11:36:50 +0700

    thay doi file hien thi

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hienthi4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblHienEmail_Click(object sender, EventArgs e)
+        {
+            // add db email
+        }
+
+        private void lblHienTK_Click(object sender, EventArgs e)
+        {
+            // add db tk
+
+        }
+    }
+}
