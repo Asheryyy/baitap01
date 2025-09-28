ec0635ba44aabe4a5cde38ba72461f3df95cb8bb
Asheryyy <24521557@gm.uit.edu,vn>
2025-09-28 09:34:01 +0700

    Thêm các file cấu hình và mã nguồn cho dự án

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace dki
+{
+    partial class frmĐK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmĐK));
+            this.lblTaiKhoan = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.lblNLMK = new System.Windows.Forms.Label();
+            this.txtDKTK = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.txtNLMK = new System.Windows.Forms.TextBox();
+            this.btnĐK = new System.Windows.Forms.Button();
+            this.chkMK = new System.Windows.Forms.CheckBox();
+            this.chkNLMK = new System.Windows.Forms.CheckBox();
+            this.picHide1 = new System.Windows.Forms.PictureBox();
+            this.picShow1 = new System.Windows.Forms.PictureBox();
+            this.picHide2 = new System.Windows.Forms.PictureBox();
+            this.picShow2 = new System.Windows.Forms.PictureBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picHide1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHide2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTaiKhoan
+            // 
+            this.lblTaiKhoan.BackColor = System.Drawing.Color.Transparent;
+            this.lblTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaiKhoan.Location = new System.Drawing.Point(50, 103);
+            this.lblTaiKhoan.Name = "lblTaiKhoan";
+            this.lblTaiKhoan.Size = new System.Drawing.Size(208, 23);
+            this.lblTaiKhoan.TabIndex = 0;
+            this.lblTaiKhoan.Text = " Tài Khoản";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.BackColor = System.Drawing.Color.Transparent;
+            this.lblMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Location = new System.Drawing.Point(50, 163);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(208, 23);
+            this.lblMatKhau.TabIndex = 1;
+            this.lblMatKhau.Text = "Mật Khẩu";
+            // 
+            // lblNLMK
+            // 
+            this.lblNLMK.BackColor = System.Drawing.Color.Transparent;
+            this.lblNLMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNLMK.Location = new System.Drawing.Point(50, 224);
+            this.lblNLMK.Name = "lblNLMK";
+            this.lblNLMK.Size = new System.Drawing.Size(208, 23);
+            this.lblNLMK.TabIndex = 2;
+            this.lblNLMK.Text = "Nhập Lại Mật Khẩu";
+            // 
+            // txtDKTK
+            // 
+            this.txtDKTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDKTK.Location = new System.Drawing.Point(307, 103);
+            this.txtDKTK.Name = "txtDKTK";
+            this.txtDKTK.Size = new System.Drawing.Size(268, 27);
+            this.txtDKTK.TabIndex = 3;
+            // 
+            // txtMK
+            // 
+            this.txtMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.Location = new System.Drawing.Point(307, 164);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(268, 27);
+            this.txtMK.TabIndex = 4;
+            this.txtMK.TextChanged += new System.EventHandler(this.txtMK_TextChanged);
+            // 
+            // txtNLMK
+            // 
+            this.txtNLMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNLMK.Location = new System.Drawing.Point(307, 225);
+            this.txtNLMK.Name = "txtNLMK";
+            this.txtNLMK.Size = new System.Drawing.Size(268, 27);
+            this.txtNLMK.TabIndex = 5;
+            this.txtNLMK.TextChanged += new System.EventHandler(this.txtNLMK_TextChanged);
+            // 
+            // btnĐK
+            // 
+            this.btnĐK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnĐK.Location = new System.Drawing.Point(323, 324);
+            this.btnĐK.Name = "btnĐK";
+            this.btnĐK.Size = new System.Drawing.Size(154, 54);
+            this.btnĐK.TabIndex = 6;
+            this.btnĐK.Text = "Đăng Kí";
+            this.btnĐK.UseVisualStyleBackColor = true;
+            this.btnĐK.Click += new System.EventHandler(this.btnĐK_Click);
+            // 
+            // chkMK
+            // 
+            this.chkMK.AutoSize = true;
+            this.chkMK.Location = new System.Drawing.Point(601, 167);
+            this.chkMK.Name = "chkMK";
+            this.chkMK.Size = new System.Drawing.Size(18, 17);
+            this.chkMK.TabIndex = 7;
+            this.chkMK.UseVisualStyleBackColor = true;
+            this.chkMK.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // chkNLMK
+            // 
+            this.chkNLMK.AutoSize = true;
+            this.chkNLMK.Location = new System.Drawing.Point(601, 229);
+            this.chkNLMK.Name = "chkNLMK";
+            this.chkNLMK.Size = new System.Drawing.Size(18, 17);
+            this.chkNLMK.TabIndex = 8;
+            this.chkNLMK.UseVisualStyleBackColor = true;
+            this.chkNLMK.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // picHide1
+            // 
+            this.picHide1.Image = ((System.Drawing.Image)(resources.GetObject("picHide1.Image")));
+            this.picHide1.Location = new System.Drawing.Point(636, 155);
+            this.picHide1.Name = "picHide1";
+            this.picHide1.Size = new System.Drawing.Size(43, 29);
+            this.picHide1.TabIndex = 9;
+            this.picHide1.TabStop = false;
+            // 
+            // picShow1
+            // 
+            this.picShow1.Image = ((System.Drawing.Image)(resources.GetObject("picShow1.Image")));
+            this.picShow1.Location = new System.Drawing.Point(636, 155);
+            this.picShow1.Name = "picShow1";
+            this.picShow1.Size = new System.Drawing.Size(43, 29);
+            this.picShow1.TabIndex = 10;
+            this.picShow1.TabStop = false;
+            this.picShow1.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // picHide2
+            // 
+            this.picHide2.Image = ((System.Drawing.Image)(resources.GetObject("picHide2.Image")));
+            this.picHide2.Location = new System.Drawing.Point(636, 224);
+            this.picHide2.Name = "picHide2";
+            this.picHide2.Size = new System.Drawing.Size(43, 33);
+            this.picHide2.TabIndex = 11;
+            this.picHide2.TabStop = false;
+            // 
+            // picShow2
+            // 
+            this.picShow2.Image = ((System.Drawing.Image)(resources.GetObject("picShow2.Image")));
+            this.picShow2.Location = new System.Drawing.Point(636, 224);
+            this.picShow2.Name = "picShow2";
+            this.picShow2.Size = new System.Drawing.Size(43, 33);
+            this.picShow2.TabIndex = 12;
+            this.picShow2.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmĐK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picShow2);
+            this.Controls.Add(this.picHide2);
+            this.Controls.Add(this.picShow1);
+            this.Controls.Add(this.picHide1);
+            this.Controls.Add(this.chkNLMK);
+            this.Controls.Add(this.chkMK);
+            this.Controls.Add(this.btnĐK);
+            this.Controls.Add(this.txtNLMK);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtDKTK);
+            this.Controls.Add(this.lblNLMK);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblTaiKhoan);
+            this.Name = "frmĐK";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Kí";
+            ((System.ComponentModel.ISupportInitialize)(this.picHide1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHide2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTaiKhoan;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.Label lblNLMK;
+        private System.Windows.Forms.TextBox txtDKTK;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.TextBox txtNLMK;
+        private System.Windows.Forms.Button btnĐK;
+        private System.Windows.Forms.CheckBox chkMK;
+        private System.Windows.Forms.CheckBox chkNLMK;
+        private System.Windows.Forms.PictureBox picHide1;
+        private System.Windows.Forms.PictureBox picShow1;
+        private System.Windows.Forms.PictureBox picHide2;
+        private System.Windows.Forms.PictureBox picShow2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
